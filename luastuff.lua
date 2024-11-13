8d2aa35f1d48937405dc7f783c495e739ffe54e1
kevinyap <kevinyap+odspmdb@microsoft.com>
2024-11-12 17:16:59 -0800

    add lazygit

--- luastuff.lua
+++ luastuff.lua
@@ -3,3 +3,12 @@ require("toggleterm").setup{
     size = 40, 
     direction = 'float',
 }
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<F4>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
